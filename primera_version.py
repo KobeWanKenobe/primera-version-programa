6c8491f3540ac627196a95d993d3f1b408cf8c96
gsantiagoperez07-hue <g.santiagoperez07@gmail.com>
2026-08-25 13:59:11 -0400

    Add files via upload

--- primera_version.py
+++ primera_version.py
@@ -1,1 +1,9 @@
 print("Hola, mi nombre es Geraldo Santiago Perez.")
+
+# Programa para mostrar el nombre y la edad
+
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+
+print("Nombre:", nombre)
+print("Edad:", edad, "años")
